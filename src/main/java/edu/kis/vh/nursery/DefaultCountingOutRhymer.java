dcbf77a04a019d1d36332b568d545f1ac4100e9b
m-skowronski <mskowronski547@gmail.com>
2022-04-21 11:36:16 +0200

    6. Ustawienie modyfikatora final przy niemutowalnych atrybutach klas

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -5,7 +5,7 @@ public class DefaultCountingOutRhymer {
     public static final int NUMBERS_SIZE = 12;
     public static final int CHECK_VALUE = -1;
     public static final int FULL_VALUE = 11;
-    private int[] numbers = new int[NUMBERS_SIZE];
+    private final int[] numbers = new int[NUMBERS_SIZE];
 
     public int total = CHECK_VALUE;
 
