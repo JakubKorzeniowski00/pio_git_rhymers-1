470d45c27bdcd738991653237eab31334eb2faa4
Jakub Korzeniowski <235898@edu.p.lodz.pl>
2022-04-28 10:55:55 +0200

    8. Zmiana modyfikatorow atrybutow z najmniejsza mozliwa widocznoscia

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,12 +2,12 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int NUMBERS_SIZE = 12;
-    public static final int CHECK_VALUE = -1;
-    public static final int FULL_VALUE = 11;
+    private static final int NUMBERS_SIZE = 12;
+    private static final int CHECK_VALUE = -1;
+    private static final int FULL_VALUE = 11;
     private final int[] numbers = new int[NUMBERS_SIZE];
 
-    public int total = CHECK_VALUE;
+    private int total = CHECK_VALUE;
 
     public void countIn(int in) {
         if (!isFull())
